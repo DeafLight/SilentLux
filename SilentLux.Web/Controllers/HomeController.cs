d70f8b539d5a9ba02a347769fbc6575dc21db06c
Xavier Lecoustillier <xavlec@gmail.com>
2018-06-30 16:07:41 +0200

    Setup dotnet watcher Add a dummy controller

---
+++ SilentLux.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SilentLux.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Home")]
+    public class HomeController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hello world from Home");
+        }
+    }
+}
